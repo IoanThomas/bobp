208641f16671310b8ac544cf9986e7eb4c540c48
Ioan Thomas <29049952+IoanThomas@users.noreply.github.com>
2022-06-29 23:26:47 +0100

    Make parsing code slightly easier to read

--- src/parse.rs
+++ src/parse.rs
@@ -1,40 +1,44 @@
 use crate::{result, Error};
-use std::ops::Deref;
 
-pub fn position(positions: &mut Vec<[f32; 3]>, tokens: &[&str]) -> result::Result<()> {
-    parse_value(positions, tokens)
+pub type Token<'a> = &'a str;
+pub type Position = [f32; 3];
+pub type TextureCoordinates = [f32; 2];
+pub type Normal = [f32; 3];
+
+pub fn position(positions: &mut Vec<Position>, tokens: &[Token]) -> result::Result<()> {
+    parse_attribute(positions, tokens)
 }
 
 pub fn texture_coordinates(
-    texture_coordinates: &mut Vec<[f32; 2]>,
-    tokens: &[&str],
+    texture_coordinates: &mut Vec<TextureCoordinates>,
+    tokens: &[Token],
 ) -> result::Result<()> {
-    parse_value(texture_coordinates, tokens)
+    parse_attribute(texture_coordinates, tokens)
 }
 
-pub fn normal(normals: &mut Vec<[f32; 3]>, tokens: &[&str]) -> result::Result<()> {
-    parse_value(normals, tokens)
+pub fn normal(normals: &mut Vec<Normal>, tokens: &[Token]) -> result::Result<()> {
+    parse_attribute(normals, tokens)
 }
 
-fn parse_value<const NUM_COMPONENTS: usize>(
-    components: &mut Vec<[f32; NUM_COMPONENTS]>,
-    tokens: &[&str],
+fn parse_attribute<const NUM_COMPONENTS: usize>(
+    attributes: &mut Vec<[f32; NUM_COMPONENTS]>,
+    tokens: &[Token],
 ) -> result::Result<()> {
-    let values = tokens
+    let components = tokens
         .iter()
-        .map(Deref::deref)
         .map(parse_float)
         .collect::<result::Result<Vec<_>>>()?;
 
-    if values.len() != NUM_COMPONENTS {
+    if components.len() != NUM_COMPONENTS {
         return Err(Error::InvalidFormat);
     }
 
-    components.push(values.try_into().map_err(|_| Error::InvalidFormat)?);
+    let attribute = components.try_into().map_err(|_| Error::InvalidFormat)?;
+    attributes.push(attribute);
 
     Ok(())
 }
 
-fn parse_float(token: &str) -> result::Result<f32> {
-    Ok(token.parse::<f32>()?)
+fn parse_float(token: impl AsRef<str>) -> result::Result<f32> {
+    Ok(token.as_ref().parse::<f32>()?)
 }
