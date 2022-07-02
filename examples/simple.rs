d6b83e72eb24a0f9570ec99d77ec3f3858a2b78a
Ioan Thomas <29049952+IoanThomas@users.noreply.github.com>
2022-07-02 12:40:56 +0100

    Add simple example

---
+++ examples/simple.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let obj_string = match std::fs::read_to_string("examples/data/plane.obj") {
+        Ok(obj_string) => obj_string,
+        Err(error) => {
+            eprintln!("Failed to load OBJ file: {}", error);
+            return;
+        }
+    };
+
+    let (vertices, indices) = match bop::parse_obj(obj_string) {
+        Ok(data) => data,
+        Err(error) => {
+            eprintln!("Failed to parse OBJ string: {}", error);
+            return;
+        }
+    };
+
+    println!("Vertices ({}): {:?}", vertices.len(), vertices);
+    println!("Indices ({}): {:?}", indices.len(), indices);
+}
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let obj_string = std::fs::read_to_string("../../plane.obj").expect("failed to load OBJ file");
-    let (vertices, indices) = bop::parse_obj(obj_string).expect("failed to parse OBJ");
-
-    println!("Vertices ({}): {:?}", vertices.len(), vertices);
-    println!("Indices ({}): {:?}", indices.len(), indices);
-}
